f453a4994b8039dbce3555b4b159ba2a3571583f
Arijit Basu <thearijitxfx@gmail.com>
2019-03-09 19:53:04 +0530

    Initial commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Arijit Basu</h1>
+      <p>Associate System Engineer, Tata Consultancy Services</p>
+      <ul>
+        <li>Mahabalipuram</li>
+        <li>Dwarka-Somnath</li>
+        <li>Marina Beach</li>
+      </ul>
+    </div>
+  );
+}
+export default MyInfo;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import MyInfo from "./components/MyInfo";
+const root = document.getElementById("root");
+
+ReactDom.render(<MyInfo />, root);
